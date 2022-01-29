7737732908e0de5db5fcc1ccf3e132e0c2828447
mrcarjul <mrcarjul@gmail.com>
2022-01-29 11:22:40 -0600

    initial version of BussinessScreen #2

---
+++ components/screens/BusinessesScreen.tsx
@@ -0,0 +1,8 @@
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import React from "react";
+import { View } from "react-native";
+import { RootStackParamList } from "../../types";
+
+export const BusinessesScreen = ({
+  navigation,
+}: NativeStackScreenProps<RootStackParamList, "Businesses">) => <View />;
