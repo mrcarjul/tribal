5d1c378bbf31533f7a026e97bfcf1ea16e2d139c
mrcarjul <mrcarjul@gmail.com>
2022-01-31 01:41:35 -0600

    corrected icon type

--- components/atoms/IconBusiness.tsx
+++ components/atoms/IconBusiness.tsx
@@ -1,6 +1,6 @@
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
 import { Icon } from "native-base";
 
 export const IconBusiness = () => (
-  <Icon size="md" as={MaterialCommunityIcons} name="business" color="black" />
+  <Icon size="md" as={MaterialIcons} name="business" color="black" />
 );
