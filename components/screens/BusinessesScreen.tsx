cffe3cbbc7e8825a9d28f4dc9c8115b53c11c43a
mrcarjul <mrcarjul@gmail.com>
2022-01-31 01:37:06 -0600

    Added initial version of Businesses screen #14

--- components/screens/BusinessesScreen.tsx
+++ components/screens/BusinessesScreen.tsx
@@ -1,8 +1,34 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import React from "react";
-import { View } from "react-native";
-import { RootStackParamList } from "../../types";
+import React, { useCallback } from "react";
+import { Box, FlatList, Spinner } from "native-base";
+import { useQuery } from "react-query";
+import { Business, RootStackParamList } from "../../types";
+import { MemoBusinessItem, Header } from "../molecules";
+import { AddButton } from "..";
+
+type Businesses = {
+  businesses: Business[];
+};
 
 export const BusinessesScreen = ({
   navigation,
-}: NativeStackScreenProps<RootStackParamList, "Businesses">) => <View />;
+}: NativeStackScreenProps<RootStackParamList, "Businesses">) => {
+  const { data, isFetching } = useQuery<Businesses>("business");
+
+  const onAddBusiness = useCallback(() => {
+    navigation.navigate("Business");
+  }, [navigation]);
+
+  return (
+    <Box flex={1}>
+      <Header title="Businesses" />
+      <FlatList
+        data={data?.businesses}
+        renderItem={({ item }) => <MemoBusinessItem {...item} />}
+        keyExtractor={(item) => item.businessId}
+        ListEmptyComponent={isFetching ? <Spinner /> : null}
+      />
+      <AddButton onPress={onAddBusiness} />
+    </Box>
+  );
+};
