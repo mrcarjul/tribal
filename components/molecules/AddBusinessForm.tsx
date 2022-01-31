367475b501aba8df247aeb853c80c2b8aa5901e5
mrcarjul <mrcarjul@gmail.com>
2022-01-31 01:27:51 -0600

    initial version of AddBusinessForm

---
+++ components/molecules/AddBusinessForm.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { VStack } from "native-base";
+import { FormikProps } from "formik";
+import { IconBusiness, BasicInput } from "../atoms";
+import { AddBusinessFormValues } from "../../types";
+
+export const AddBusinessForm = ({
+  values,
+  touched,
+  errors,
+  handleChange,
+  handleBlur,
+}: FormikProps<AddBusinessFormValues>) => {
+  return (
+    <VStack space={3} mt="5" width="100%">
+      <BasicInput
+        placeholder="Business name"
+        error={touched.name ? errors.name : undefined}
+        onBlur={handleBlur("name")}
+        onChangeText={handleChange("name")}
+        testID="add-bussines-input-name"
+        InputLeftElement={<IconBusiness />}
+        value={values.name}
+      />
+    </VStack>
+  );
+};
