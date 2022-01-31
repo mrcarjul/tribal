d86a1b40de1e430aeb85f8cc99b286a18ea1f7ba
mrcarjul <mrcarjul@gmail.com>
2022-01-31 03:29:55 -0600

    initial version of mutation

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,2 +1,3 @@
 export * from "./useBusiness";
+export * from "./useBusinessCreate";
 export * from "./useCachedResources";
---
+++ hooks/useBusinessCreate.ts
@@ -0,0 +1,54 @@
+import { Alert } from "react-native";
+import { useMutation } from "react-query";
+import { API, businessEndpoints, queryClient } from "../services";
+import { Businesses } from "../types";
+
+/**
+ * @description Manages post of a business with useMutation
+ * @param {cb} onSuccessEnd to be executed at the end of onSuccess
+ * @param {cb} onError
+ * @returns
+ */
+export const useBusinessCreate = ({
+  onSuccessEnd,
+  onError,
+}: {
+  onSuccessEnd: () => void;
+  onError: () => void;
+}) => {
+  return useMutation(
+    (name: string) => businessEndpoints(API).addBusiness(name),
+    {
+      onSuccess: (response, name) => {
+        if (response.status === 200) {
+          const businessId = response.headers["x-amzn-requestid"];
+          if (typeof businessId === "string") {
+            queryClient.setQueryData(["business"], (currentBusinessesData) => {
+              const { businesses } = currentBusinessesData as Businesses;
+              const newBusiness = {
+                businessId,
+                name,
+              };
+              if (businesses?.length) {
+                return {
+                  businesses: [...businesses, newBusiness],
+                };
+              }
+              return {
+                businesses: [newBusiness],
+              };
+            });
+            Alert.alert("Business added");
+            onSuccessEnd();
+            return;
+          }
+          onError();
+        }
+      },
+      onError,
+      onSettled: () => {
+        queryClient.invalidateQueries("business");
+      },
+    }
+  );
+};
