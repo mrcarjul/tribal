e2eda6a3a376e677789ac19911dce489e6a1a3a1
mrcarjul <mrcarjul@gmail.com>
2022-01-31 01:23:00 -0600

    added default export for IconBusiness

--- components/atoms/index.ts
+++ components/atoms/index.ts
@@ -2,4 +2,5 @@ export * from "./AddButton";
 export * from "./ArrowBackButton";
 export * from "./BasicInput";
 export * from "./EditButton";
+export * from "./IconBusiness";
 export * from "./Title";
